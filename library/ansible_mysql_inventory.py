ff408557558e78d066fda7913760c824e51627e9
Larry Smith Jr <mrlesmithjr@gmail.coml>
2016-05-22 15:37:13 -0400

    Added var parsing

--- library/ansible_mysql_inventory.py
+++ library/ansible_mysql_inventory.py
@@ -5,6 +5,8 @@
 # @mrlesmithjr
 # mrlesmithjr@gmail.com
 
+# Import modules
+import argparse
 import datetime
 import MySQLdb
 import sys
@@ -13,17 +15,20 @@ try:
 except ImportError:
     import simplejson as json
 
+# Setup arguments
+parser = argparse.ArgumentParser(description='Ansible Inventory...')
+parser.add_argument('-H','--host', help='Database Host',required=True)
+parser.add_argument('-U','--user', help='Database User',required=True)
+parser.add_argument('-P','--password', help='Database Password',required=True)
+args = parser.parse_args()
+
 # Define Vars
-db_host = 'localhost'
 db_name = 'ansible_inventory'
-db_pass = 'ansible'
 db_query = 'SELECT HostName,AnsibleSSHHost,HostDistribution,HostDistributionRelease,HostDistributionVersion,GroupName FROM inventory'
-#db_query = 'SELECT * FROM inventory'
 db_table = 'inventory'
-db_user = 'ansible'
 
 # Setup Connection
-con = MySQLdb.connect(db_host, db_user, db_pass, db_name);
+con = MySQLdb.connect(args.host, args.user, args.password, db_name);
 
 cur = con.cursor()
 cur.execute(db_query)
@@ -36,7 +41,6 @@ cur.execute(db_query)
 for HostName, AnsibleSSHHost, HostDistribution, HostDistributionRelease, HostDistributionVersion, GroupName in cur:
 #    print("inventory_hostname: {}, ansible_ssh_host: {}, group_names: {}").format(HostName,AnsibleSSHHost,GroupName)
     print(json.dumps({'inventory_hostname': HostName, 'ansible_ssh_host': AnsibleSSHHost, 'ansible_distribution': HostDistribution, \
-    'ansible_distribution_release': HostDistributionRelease, 'ansible_distribution_version': HostDistributionVersion, 'group_names': GroupName}, \
-    sort_keys=True, indent=4))
+    'ansible_distribution_release': HostDistributionRelease, 'ansible_distribution_version': HostDistributionVersion, 'group_names': GroupName}))
 cur.close()
 con.close()
