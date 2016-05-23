ded4f4f20775c7d276f1842c11d6cbf1d5e7c2c2
Larry Smith Jr <mrlesmithjr@gmail.com>
2016-05-22 22:17:16 -0400

    Updated python script

---
+++ library/ansible_inventory.py
@@ -0,0 +1,79 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Larry Smith Jr.
+# http://everythingshouldbevirtual.com
+# @mrlesmithjr
+# mrlesmithjr@gmail.com
+
+# Import modules
+import argparse
+import datetime
+import MySQLdb
+import sys
+try:
+    import json
+except ImportError:
+    import simplejson as json
+
+# Define classes
+class DatetimeEncoder(json.JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, datetime):
+            return obj.strftime('%Y-%m-%dT%H:%M:%SZ')
+        elif isinstance(obj, date):
+            return obj.strftime('%Y-%m-%d')
+        # Let the base class default method raise the TypeError
+        return json.JSONEncoder.default(self, obj)
+
+# Setup arguments
+parser = argparse.ArgumentParser(description='Ansible Inventory...')
+parser.add_argument('-F','--function', help='Function to Execute, [default: all], valid choices: [groups | hosts | all]',default='all',required=False)
+parser.add_argument('-H','--host', help='Database Host, [default: 127.0.0.1]',default='127.0.0.1',required=False)
+parser.add_argument('-P','--password', help='Database Password',required=True)
+parser.add_argument('-U','--user', help='Database User',required=True)
+args = parser.parse_args()
+
+# Define Vars
+db_name = 'ansible_inventory'
+
+# Defined functions
+def all_groups():
+    db_query = 'SELECT GroupName FROM inventory'
+    con = MySQLdb.connect(args.host, args.user, args.password, db_name);
+    cur = con.cursor()
+    cur.execute(db_query)
+    rows = cur.fetchall()
+    for row in rows:
+        print(json.dumps(row))
+    cur.close()
+    con.close()
+
+def all_hosts():
+    db_query = 'SELECT HostName FROM inventory'
+    con = MySQLdb.connect(args.host, args.user, args.password, db_name);
+    cur = con.cursor()
+    cur.execute(db_query)
+    rows = cur.fetchall()
+    for row in rows:
+        print(json.dumps(row))
+    cur.close()
+    con.close()
+
+def all_inventory():
+    db_query = 'SELECT HostName,AnsibleSSHHost,HostDistribution,HostDistributionRelease,HostDistributionVersion,GroupName FROM inventory'
+    con = MySQLdb.connect(args.host, args.user, args.password, db_name);
+    cur = con.cursor()
+    cur.execute(db_query)
+    rows = cur.fetchall()
+    for row in rows:
+        print(json.dumps(row))
+    cur.close()
+    con.close()
+
+# Decide which function to execute
+if args.function == "all":
+    all_inventory()
+elif args.function == "groups":
+    all_groups()
+elif args.function == "hosts":
+    all_hosts()
