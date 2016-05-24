534b8dd850e12ad53717b19d4ac7ca5865f030ff
Larry Smith Jr <mrlesmithjr@gmail.com>
2016-05-24 14:36:53 -0400

    Cleaned up long lines
    Signed-off-by: Larry Smith Jr <mrlesmithjr@gmail.com>

--- library/ansible_inventory.py
+++ library/ansible_inventory.py
@@ -5,36 +5,28 @@
 # @mrlesmithjr
 # mrlesmithjr@gmail.com
 
+from __future__ import print_function
+
 # Import modules
 import argparse
-import datetime
+import json
 import MySQLdb
-import sys
-try:
-    import json
-except ImportError:
-    import simplejson as json
-
-# Define classes
-class DatetimeEncoder(json.JSONEncoder):
-    def default(self, obj):
-        if isinstance(obj, datetime):
-            return obj.strftime('%Y-%m-%dT%H:%M:%SZ')
-        elif isinstance(obj, date):
-            return obj.strftime('%Y-%m-%d')
-        # Let the base class default method raise the TypeError
-        return json.JSONEncoder.default(self, obj)
 
 # Setup arguments
 parser = argparse.ArgumentParser(description='Ansible Inventory...')
-parser.add_argument('--addhost', help='Add Hostname',required=False)
-parser.add_argument('--function', help='Function to Execute, [default: all], valid choices: \
-[addhost | all | groups | hosts | querygroup | queryhost]',default='all',required=False)
-parser.add_argument('--host', help='Database Host, [default: 127.0.0.1]',default='127.0.0.1',required=False)
-parser.add_argument('--password', help='Database Password',required=True)
-parser.add_argument('--querygroup', help='Query Group, Define Group to Query',required=False)
-parser.add_argument('--queryhost', help='Query Host, Define Host to Query',required=False)
-parser.add_argument('--user', help='Database User',required=True)
+parser.add_argument('--addhost', required=False, help='Add Hostname')
+parser.add_argument('--function', default='all', required=False,
+                    help='Function to Execute...\n'
+                    'valid function choices are\n'
+                    '[addhost | all | groups | hosts | querygroup | queryhost]')
+parser.add_argument('--host', default='127.0.0.1', required=False,
+                    help='Database Host, [default: 127.0.0.1]')
+parser.add_argument('--password', required=True, help='Database Password')
+parser.add_argument('--querygroup', required=False,
+                    help='Query Group, Define Group to Query')
+parser.add_argument('--queryhost', required=False,
+                    help='Query Host, Define Host to Query')
+parser.add_argument('--user', required=True, help='Database User')
 args = parser.parse_args()
 
 # Define Vars
@@ -42,8 +34,8 @@ db_name = 'ansible_inventory'
 
 # Defined functions
 def add_host():
-    sql = ('INSERT INTO Hosts(HostName) VALUES("%s")' %(args.addhost))
-    con = MySQLdb.connect(args.host, args.user, args.password, db_name);
+    sql = "INSERT INTO Hosts(HostName) VALUES('%s')" %(args.addhost)
+    con = MySQLdb.connect(args.host, args.user, args.password, db_name)
     cur = con.cursor()
     try:
         cur.execute(sql)
@@ -55,8 +47,8 @@ def add_host():
         print(e)
 
 def all_groups():
-    sql = 'SELECT DISTINCT GroupName FROM Groups'
-    con = MySQLdb.connect(args.host, args.user, args.password, db_name);
+    sql = "SELECT DISTINCT GroupName FROM Groups"
+    con = MySQLdb.connect(args.host, args.user, args.password, db_name)
     cur = con.cursor()
     cur.execute(sql)
     rows = cur.fetchall()
@@ -70,8 +62,8 @@ def all_groups():
     con.close()
 
 def all_hosts():
-    sql = 'SELECT DISTINCT HostName FROM Hosts'
-    con = MySQLdb.connect(args.host, args.user, args.password, db_name);
+    sql = "SELECT DISTINCT HostName FROM Hosts"
+    con = MySQLdb.connect(args.host, args.user, args.password, db_name)
     cur = con.cursor()
     cur.execute(sql)
     rows = cur.fetchall()
@@ -85,25 +77,31 @@ def all_hosts():
     con.close()
 
 def all_inventory():
-    sql = 'SELECT HostName,AnsibleSSHHost,HostDistribution,HostDistributionRelease,HostDistributionVersion,GroupName \
-    FROM inventory'
-    con = MySQLdb.connect(args.host, args.user, args.password, db_name);
+    sql = """
+        SELECT HostName,AnsibleSSHHost,HostDistribution,
+        HostDistributionRelease,HostDistributionVersion,
+        GroupName FROM inventory"""
+    con = MySQLdb.connect(args.host, args.user, args.password, db_name)
     cur = con.cursor()
     cur.execute(sql)
     rows = cur.fetchall()
     results = []
     results.append('inventory')
-    for HostName, AnsibleSSHHost, HostDistribution, HostDistributionRelease, HostDistributionVersion, GroupName in rows:
-        results.append({'host': HostName, 'ansible_ssh_host': AnsibleSSHHost, 'ansible_distribution': HostDistribution, \
-        'ansible_distribution_release': HostDistributionRelease, 'ansible_distribution_version': HostDistributionVersion, \
+    for HostName, AnsibleSSHHost, HostDistribution, HostDistributionRelease, \
+        HostDistributionVersion, GroupName in rows: \
+        results.append({'host': HostName, 'ansible_ssh_host': AnsibleSSHHost, \
+        'ansible_distribution': HostDistribution, \
+        'ansible_distribution_release': HostDistributionRelease, \
+        'ansible_distribution_version': HostDistributionVersion, \
         'groups': GroupName})
     print(json.dumps(results, sort_keys=True))
     cur.close()
     con.close()
 
 def query_group():
-    sql = ('SELECT HostName,AnsibleSSHHost FROM inventory WHERE GroupName="%s" ORDER BY HostName' %(args.querygroup))
-    con = MySQLdb.connect(args.host, args.user, args.password, db_name);
+    sql = """SELECT HostName,AnsibleSSHHost FROM inventory WHERE
+        GroupName='%s' ORDER BY HostName""" %(args.querygroup)
+    con = MySQLdb.connect(args.host, args.user, args.password, db_name)
     cur = con.cursor()
     cur.execute(sql)
     rows = cur.fetchall()
@@ -116,15 +114,17 @@ def query_group():
     con.close()
 
 def query_host():
-    sql = ('SELECT HostName,AnsibleSSHHost,GroupName FROM inventory WHERE HostName="%s"' %(args.queryhost))
-    con = MySQLdb.connect(args.host, args.user, args.password, db_name);
+    sql = """SELECT HostName,AnsibleSSHHost,GroupName FROM inventory
+        WHERE HostName='%s'""" %(args.queryhost)
+    con = MySQLdb.connect(args.host, args.user, args.password, db_name)
     cur = con.cursor()
     cur.execute(sql)
     rows = cur.fetchall()
     results = []
     results.append('inventory')
     for HostName, AnsibleSSHHost, GroupName in rows:
-        results.append({'host': HostName, 'ansible_ssh_host': AnsibleSSHHost, 'groups': GroupName})
+        results.append({'host': HostName, 'ansible_ssh_host': AnsibleSSHHost,
+                        'groups': GroupName})
     print(json.dumps(results, sort_keys=True))
     cur.close()
     con.close()
