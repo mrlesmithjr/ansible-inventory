b410ef70fe32621030ccf4cdb8fc11b0d4ec80e9
Larry Smith Jr <mrlesmithjr@gmail.com>
2016-06-05 17:33:42 -0400

    Rewrote command line options
    Signed-off-by: Larry Smith Jr <mrlesmithjr@gmail.com>

--- library/ansible_inventory.py
+++ library/ansible_inventory.py
@@ -22,12 +22,9 @@ class AnsibleMySQL(object):
     """
     Setup Main execution
     """
-
     def __init__(self):
-
         self.read_cli_args()
-
-        if self.args.all:
+        if self.args.action == "queryall":
             self.sql = """
                 SELECT h.inventory_hostname, hg.group_names, hd.*
                 FROM
@@ -35,41 +32,41 @@ class AnsibleMySQL(object):
                 WHERE h.HostId = hd.HostId AND h.HostId = hg.HostId
                 ORDER BY h.inventory_hostname
                 """
-        elif self.args.allgroups:
+        elif self.args.action == "queryallgroups":
             self.sql = """
                 SELECT DISTINCT `group_names`
                 FROM Groups
                 ORDER BY group_names
                 """
-        elif self.args.allhosts:
+        elif self.args.action == "queryallhosts":
             self.sql = """
                 SELECT DISTINCT `inventory_hostname`
                 FROM Hosts
                 ORDER BY inventory_hostname
                 """
-        elif self.args.querygroup:
+        elif self.args.action == "querygroup":
             self.sql = """
                 SELECT hg.group_names, h.inventory_hostname, hd.ansible_hostname, hd.ansible_ssh_host
                 FROM
                 HostDetails AS hd, Hosts AS h, HostGroups AS hg
                 WHERE h.HostId = hd.HostId AND h.HostId = hg.HostId AND hg.group_names = "%s"
                 ORDER BY h.inventory_hostname
-                """ %(self.args.querygroup)
-        elif self.args.queryhost:
+                """ %(self.args.group)
+        elif self.args.action == "queryhost":
             self.sql = """
                 SELECT h.inventory_hostname, hd.ansible_hostname, hd.ansible_ssh_host, hg.group_names
                 FROM
                 HostDetails AS hd, Hosts AS h, HostGroups AS hg
                 WHERE h.HostId = hd.HostId AND h.HostId = hg.HostId AND h.inventory_hostname = "%s"
                 ORDER BY hg.group_names
-                """ %(self.args.queryhost)
-        elif self.args.queryhostdetails:
+                """ %(self.args.host)
+        elif self.args.action == "queryhostdetails":
             self.sql = """
                 SELECT *
                 FROM HostDetails
                 WHERE HostId IN
                 (SELECT HostID FROM Hosts WHERE inventory_hostname = '%s')
-                """ %(self.args.queryhostdetails)
+                """ %(self.args.host)
         else:
             self.sql = """
                 SELECT h.inventory_hostname, hg.group_names, hd.*
@@ -77,7 +74,6 @@ class AnsibleMySQL(object):
                 HostDetails AS hd, Hosts AS h, HostGroups AS hg
                 WHERE h.HostId = hd.HostId AND h.HostId = hg.HostId
                 """
-
         self.gather_inventory()
         self.process_results()
 
@@ -86,8 +82,8 @@ class AnsibleMySQL(object):
         Gather inventory from MySQL based on query
         """
         try:
-            self.con = MySQLdb.connect(self.args.host, self.args.user,
-                                       self.args.password, self.args.db)
+            self.con = MySQLdb.connect(self.args.dbhost, self.args.dbuser,
+                                       self.args.dbpassword, self.args.dbname)
             self.cur = self.con.cursor()
             self.cur.execute(self.sql)
             self.rows = self.cur.fetchall()
@@ -96,51 +92,39 @@ class AnsibleMySQL(object):
             self.con.close()
 
     def process_results(self):
-
         """
         Process and display results of the query
         """
-
         self.columns = [desc[0] for desc in self.cur.description]
         self.results = []
         for self.row in self.rows:
             self.row = dict(zip(self.columns, self.row))
             self.results.append(self.row)
-        if self.args.prettyprint:
-            print json.dumps(self.results, default=datetime_handler,
-                             sort_keys=True, indent=2, separators=(',', ': '))
-        elif not self.args.prettyprint:
-            print json.dumps(self.results, default=datetime_handler,
-                             sort_keys=True)
+        print json.dumps(self.results, default=datetime_handler, indent=4)
 
     def read_cli_args(self):
         """
         Setup and Read Command Line Arguments to Pass
         """
-
         parser = argparse.ArgumentParser(description='Ansible Inventory...')
-        parser.add_argument('--all', help='Display all inventory items',
-                            action='store_true')
-        parser.add_argument('--allgroups', help='Display all groups',
-                            action='store_true')
-        parser.add_argument('--allhosts', help='Display all hosts',
-                            action='store_true')
-        parser.add_argument('--db', default='ansible_inventory',
-                            help='Database Name')
-        parser.add_argument('--host', default='127.0.0.1',
+        parser.add_argument('action', help='Define action to take',
+                            choices=['queryall', 'queryallgroups', 'queryallhosts',
+                                     'querygroup', 'queryhost', 'queryhostdetails'])
+        parser.add_argument('--dbhost', default='127.0.0.1',
                             help='Database Host, [default: 127.0.0.1]')
-        parser.add_argument('--password', required=True,
+        parser.add_argument('--dbname', default='ansible_inventory',
+                            help='Database Name, [default: ansible_inventory]')
+        parser.add_argument('--dbpassword', required=True,
                             help='Database Password')
-        parser.add_argument('--prettyprint', default=False, help='Print pretty JSON',
-                            action='store_true')
-        parser.add_argument('--querygroup',
-                            help='Query Group, Define Group to Query')
-        parser.add_argument('--queryhost',
-                            help='Query Host, Define Host to Query')
-        parser.add_argument('--queryhostdetails',
-                            help='Query Host Full Details, Define Host to Query')
-        parser.add_argument('--user', required=True, help='Database User')
+        parser.add_argument('--dbuser', required=True, help='Database User')
+        parser.add_argument('--group', help='Query Group, Define Group to Query')
+        parser.add_argument('--host', help='Query Host, Define Host to Query')
         self.args = parser.parse_args()
+        if (self.args.action == "queryhost" or
+                self.args.action == "queryhostdetails") and self.args.host is None:
+            parser.error("--host is required")
+        if self.args.action == "querygroup" and self.args.group is None:
+            parser.error("--group is required")
 
 def datetime_handler(obj):
     """
